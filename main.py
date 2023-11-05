af23d0de8fbd6459bb2e5c1d191d92d8f04e8986
Bassem Debbabi <bassem.debbabi@atos.net>
2023-11-05 23:51:05 +0100

    Add feature2_function1 to main.py
     rebasage interactif en cours ; sur 6521715
    	modifié :         main.py

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ def greet_user(username):
     print(f"Hello, {username}! We hope you enjoy the tutorial!")
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 def feature3_function1():
     print("This is a feature3 function1.")
+
+def feature2_function2():
+    print("This is a feature2 function2.")
 =======
 def feature2_function1():
     print("This is a feature2 function1.")
 >>>>>>> 75f96dc (Add feature2_function1 to main.py)
-
-def feature2_function2():
-    print("This is a feature2 function2.")
 
 if __name__ == "__main__":
     main()
